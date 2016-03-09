273651781e7c19151c2a439b8693b2b38a663943
jesusArnas <jesus.arnas@gmail.com>
2016-03-09 15:20:56 +0100

    Mi solucion a la kata
    Solución siguiendo los siguientes pasos:
    - extraer a metodos la identificacion de items por name
    - extraer a metodos el manejo de quality y sellIn
    - cambiar la lógica de los condicionales de negativa a afirmativa
    - agrupar por tipo de cerveza
    - dividir el método en manejo del quality y manejo del sellIn

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -7,74 +7,83 @@ function Item(name, sell_in, quality) {
 var items = [];
 
 function update_quality() {
-  "use strict";
-  items.forEach(function (item) {
-    if (!isAgedBrie(item) && !isBackstagePass(item)) {
-      if (isQualityPositive(item) && !isSulfuras(item)) {
-          decrementQuality(item);
-      }
-    } else {
-      if (isQualityLimit(item)) {
-        incrementQuality(item);
-        if (isBackstagePass(item) && isQualityLimit(item)) {
-          if (item.sell_in < 11) {
-              incrementQuality(item);
-          }
-          if (item.sell_in < 6) {
-              incrementQuality(item);
-          }
-        }
-      }
-    }
-    if (!isSulfuras(item)) {
-      item.sell_in = item.sell_in - 1;
-    }
-    if (item.sell_in < 0) {
-      if (!isAgedBrie(item)) {
-        if (!isBackstagePass(item) && isQualityPositive(item) && !isSulfuras(item)) {
-          decrementQuality(item);
-        } else {
-          item.quality = 0;
-        }
-      } else {
-        if (isQualityLimit(item)) {
-          incrementQuality(item);
-        }
-      }
-    }
-  });
+  var item;
+  for (var i = 0; i < items.length; i++) {
+    item = items[i];
 
-
+    updateQuality(item);
+    updateSellIn(item);
+  }
+  
   function isAgedBrie(item) {
-
-    return item.name === 'Aged Brie';
+    return item.name == 'Aged Brie';
   }
-
+  
+  function isBackstage(item) {
+    return item.name == 'Backstage passes to a TAFKAL80ETC concert';
+  }
+  
   function isSulfuras(item) {
-    return item.name === 'Sulfuras, Hand of Ragnaros';
+    return item.name == 'Sulfuras, Hand of Ragnaros';
+  }
+  
+  function decreaseQuality(item) {
+    if (item.quality > 0)
+      item.quality = item.quality - 1;
+  }
+  
+  function increaseQuality(item) {
+    if (item.quality < 50)
+      item.quality = item.quality + 1;
   }
 
-  function isBackstagePass(item) {
-    return item.name === 'Backstage passes to a TAFKAL80ETC concert';
+  function decreaseSellIn(item) {
+    item.sell_in = item.sell_in - 1;
   }
-
-  function incrementQuality(item){
-    item.quality ++;
+    
+  function resetQuality(item) {
+    item.quality = item.quality - item.quality;
   }
+  
+  function updateQuality(item) {
+    if (isAgedBrie(item)) {
+      increaseQuality(item);
+    }
+    else if (isBackstage(item)) {
+      increaseQuality(item);
 
-  function decrementQuality(item){
-    item.quality --;
+      if (item.sell_in < 11)
+        increaseQuality(item);
+       
+      if (item.sell_in < 6)
+        increaseQuality(item);
+    }
+    else if (isSulfuras(item)) {
+      // Nothing to do.
+    }
+    else {
+       decreaseQuality(item);
+    }
   }
-
-  function isQualityPositive(item){
-    return item.quality > 0;
-  }
-
-  function isQualityLimit(item){
-    return item.quality < 50;
-  }
-
-  function isSellEarly(item){
-    return item.sell_in < 11;
+  
+  function updateSellIn(item) {
+    if (isAgedBrie(item)) {
+      decreaseSellIn(item);
+      if (item.sell_in < 0)
+        increaseQuality(item);
+    }
+    else if (isBackstage(item)) {
+      decreaseSellIn(item);
+      if (item.sell_in < 0)
+        resetQuality(item);
+    }
+    else if (isSulfuras(item)) {
+      // Nothing to do.
+    }
+    else {
+       decreaseSellIn(item);
+       if (item.sell_in < 0)
+         decreaseQuality(item);
+    }
   }
 }
